dca9e4ea5ab030fa6cb306cb86f0eb7b15639f1f
Herick Alexandre Correia Santos <Herick386@gmail.com>
2025-04-16 19:44:54 -0400

    Delete parallax.jsx

--- parallax.jsx
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', () => {
-    const scrollY = window.scrollY;
-    const background = document.body;
-  
-    // Move o fundo mais devagar que o scroll
-    background.style.setProperty('--parallax-offset', `${scrollY * 0.4}px`);
-  });
-  
